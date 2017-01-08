044017f95871c37758f3a7cc20866afd879567cc
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-01-08 14:28:33 +0000

    Insert challenge solution

---
+++ mongoInsert.js
@@ -0,0 +1,25 @@
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/learnyoumongo';
+var firstName = process.argv[2];
+var lastName = process.argv[3];
+
+mongo.connect(url, function(err, db) {
+    if(err) throw err;
+
+    var docs = db.collection('docs');
+
+    myDoc = {
+        firstName: firstName,
+        lastName: lastName      
+    };
+
+    docs.insert(myDoc, function(err, data) {
+            if(err) throw err;
+
+            console.log(JSON.stringify(myDoc));
+
+            db.close(function(err) {
+                if(err) throw err;
+        });
+    })
+});

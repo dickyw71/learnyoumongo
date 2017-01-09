c66b063ba990475d98eecc29028030f780eae8fa
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-01-09 21:12:36 +0000

    Added comment to explain the exercise.

--- mongoFind.js
+++ mongoFind.js
@@ -1,3 +1,8 @@
+/** 
+* Use the parrots collection to find all documents where age
+* is greater than the first argument passed to your script.
+*/
+
 var mongo = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/learnyoumongo';
 var age = parseInt(process.argv[2], 10);
